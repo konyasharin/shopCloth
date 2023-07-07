83f3d11f0a28efe86adc1e9d637743867bf87a58
Nikolai Malyshev <kolechka.malyshev.04@gmail.com>
2023-07-07 09:00:13 +0700

    final

--- js/script.js
+++ js/script.js
@@ -1,52 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const burger = document.querySelector(".burger"),
-          header = document.querySelector(".header")
-          burgerSticks = document.querySelectorAll(".burger__stick");
-    let sticksList = [],
-        allTime,
-        start,
-        end,
-        counterBurger = 0;
-    burgerSticks.forEach((stick) => {
-        sticksList.push(stick);
+          headerLinks = document.querySelector('.header__links')
+    burger.addEventListener('click', () => {
+        headerLinks.classList.toggle('header__links_active')
     })
-    function burgerClose(){
-        sticksList[1].classList.toggle("burger__stick_show");
-        sticksList[1].classList.toggle("burger__stick_hide");
-        start = new Date();
-        allTime = 10*45
-        animationCloseTimer = setInterval(animationClose, 10);
-        burger.removeEventListener("click", burgerClose);
-        burger.addEventListener("click", clickBurger);
-    }
-    function animationClose(){
-        counterBurger -= 1
-        sticksList[0].style.transform = `rotate(${counterBurger}deg)`;
-        sticksList[2].style.transform = `rotate(-${counterBurger}deg)`;
-        sticksList[2].style.bottom = "0px";
-        end = new Date;
-        if(end - start >= allTime){
-            clearInterval(animationCloseTimer);
-        }
-    }
-    function clickBurger(){
-        sticksList[1].classList.toggle("burger__stick_show");
-        sticksList[1].classList.toggle("burger__stick_hide");
-        start = new Date();
-        allTime = 10*45
-        animationBurgerTimer = setInterval(animationBurger, 10);
-        burger.removeEventListener("click", clickBurger);
-        burger.addEventListener("click", burgerClose);
-    }
-    function animationBurger(){
-        counterBurger += 1
-        sticksList[0].style.transform = `rotate(${counterBurger}deg)`;
-        sticksList[2].style.transform = `rotate(-${counterBurger}deg)`;
-        sticksList[2].style.bottom = "14px";
-        end = new Date;
-        if(end - start >= allTime){
-            clearInterval(animationBurgerTimer);
-        }
-    }
-    burger.addEventListener("click", clickBurger);
 })
